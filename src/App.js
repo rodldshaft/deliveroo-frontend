ea6f859357d675e25272922404b6722a0d0e6cab
Gérold <gerold380@hotmail.com>
2022-07-25 17:08:53 +0200

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
@@ -7,10 +7,14 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
-    const response = await axios.get("http://localhost:3100/");
-    // console.log(response.data);
-    setData(response.data);
-    setIsLoading(false);
+    try {
+      const response = await axios.get("http://localhost:3200/");
+      // console.log(response.data);
+      setData(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log();
+    }
   };
 
   useEffect(() => {
@@ -20,7 +24,36 @@ function App() {
   return isLoading ? (
     <span>En cours de chargement... </span>
   ) : (
-    <h2>{data.restaurant.name}</h2>
+    <body>
+      <header>
+        <h2>{data.restaurant.name}</h2>
+        <div>
+          <p>{data.restaurant.description}</p>
+          <img src={data.restaurant.picture} alt="header image" />
+        </div>
+      </header>
+      <section>
+        {data.categories.map((categories, index) => {
+          // console.log(categories.meals[index]);
+          return isLoading ? (
+            <span>En cours de chargement... </span>
+          ) : (
+            <div>
+              <h3 key={index}>{categories.name}</h3>
+              {data.categories.map((meals, index) => {
+                // console.log(meals[index]);
+                return isLoading ? (
+                  <span>En cours de chargement... </span>
+                ) : (
+                  <h4 key={index}>{meals[index].title}</h4>
+                );
+              })}
+              {/* <p>{categories.meals[index].decription}</p> */}
+            </div>
+          );
+        })}
+      </section>
+    </body>
   );
 }
 
